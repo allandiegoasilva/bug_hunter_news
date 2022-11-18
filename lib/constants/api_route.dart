c09ed04cd5f282f1e17a7233e5100182ee836ed2
unknown <allandiego252@gmail.com>
2022-11-18 20:15:12 -0300

    :rocket: include api new routes

--- lib/constants/api_route.dart
+++ lib/constants/api_route.dart
@@ -3,4 +3,9 @@ class ApiRoute {
   static const String authenticate = "/user/auth";
   static const String register = "/user/register";
   static const String user_platform = "/user/platform/";
+  static const String user_platform_create = "/user/platform/create";
+  static const String user_platform_upate = "/user/platform/update";
+  static const String program = "/program";
+  static const String program_favorite = "/program/favorite/";
+  static const String remove_program_favorite = "/program/favorite/";
 }
